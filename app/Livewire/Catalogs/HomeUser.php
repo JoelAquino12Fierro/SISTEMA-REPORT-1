83128b1968eebe7939dabc8f08b918176ba8c9cf
joelaquino12fierro <joelaquinof@gmail.com>
2025-03-06 14:12:48 -0600

    primer cambio desarrollo

---
+++ app/Livewire/Catalogs/HomeUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Catalogs;
+
+use Livewire\Component;
+
+class HomeUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.catalogs.home-user');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,30 @@
 <?php
 
+use App\Livewire\Catalogs\HomeUser;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/adminUser', HomeUser::class)->name('afac.users');
     Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+                return view('dashboard');
+            })->name('dashboard');
+
+    // Route::group(['middleware' => ['role:siafac_super_admin']], function () {
+
+    //     Route::get('/adminUser', HomeUser::class)->name('afac.users');
+
+    // });
 });
